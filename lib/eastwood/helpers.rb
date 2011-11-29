e78b02e7d5c481d3955b5b5bcbb398af97a45b01
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-11-28 16:20:49 -0800

    Handle root path as a special case in routes

--- lib/eastwood/helpers.rb
+++ lib/eastwood/helpers.rb
@@ -1,23 +1,23 @@
 module Eastwood
   module Helpers
     extend ActiveSupport::Concern
-    
+
     module InstanceMethods
-      
+
       def application_name
-        app.engine_name.camelize.delete 'Application'
+        app.engine_name.camelize
       end
-      
+
       def env
         Rails.env
       end
-      
+
       def named_routes
         app.routes.routes.select { |r| r.name.present? }
       end
-      
+
       protected
-      
+
       def app
         Rails.application
       end
