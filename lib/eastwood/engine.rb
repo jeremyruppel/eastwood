eb1715a2cb6644a63d05dfa4c01d039075507d6b
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-02-25 10:31:28 -0800

    Watch for changes in eastwood initializer

--- lib/eastwood/engine.rb
+++ lib/eastwood/engine.rb
@@ -2,9 +2,12 @@ module Eastwood
   class Engine < Rails::Engine
 
     initializer 'eastwood.setup' do |app|
+      # include helpers in the sprockets context
       app.assets.context_class.instance_eval do
         include Eastwood::Context
       end
+      # watch for changes in eastwood initializer
+      app.config.watchable_files << 'config/initializers/eastwood.rb'
     end
   end
 end
