aa608d3f4aadc93a59ab35c1b62aa39d05db6d58
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-05-21 14:39:43 -0700

    Add basic specs around i18n

---
+++ spec/requests/i18n_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'eastwood/i18n.js' do
+
+  before do
+    get '/assets/eastwood/i18n.js'
+  end
+
+  let( :context   ){ ExecJS.compile response.body          }
+  let( :namespace ){ "window.#{Eastwood.application_name}" }
+
+  describe 'the response' do
+    subject { response }
+
+    its( :status ){ should eq( 200 ) }
+    its( :content_type ){ should eq( 'text/javascript' ) }
+  end
+
+  describe 'locale' do
+    it 'should be "en"' do
+      context.eval( "#{namespace}.I18n.locale" ).should eq( 'en' )
+    end
+  end
+
+  describe 'translations' do
+    it 'should be defined' do
+      context.eval( "typeof #{namespace}.I18n.en" ).should eq( 'object' )
+    end
+
+    it 'should provide hello world' do
+      context.eval( "#{namespace}.I18n.t( 'hello' )" ).should eq( 'Hello world' )
+    end
+  end
+end
