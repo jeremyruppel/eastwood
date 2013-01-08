24633c41de91b5b6e09c95b708a0dd630dc6a336
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2013-01-08 15:13:29 -0800

    Run eastwood initializer for all gem groups

--- lib/eastwood/engine.rb
+++ lib/eastwood/engine.rb
@@ -1,7 +1,7 @@
 module Eastwood
   class Engine < ::Rails::Engine
 
-    initializer 'eastwood.setup' do |app|
+    initializer 'eastwood.setup', :group => :all do |app|
       # include helpers in the sprockets context
       app.assets.context_class.instance_eval do
         include Eastwood::Context
